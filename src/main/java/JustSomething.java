be38f75124224a546196b24303689fc75b11dabf
DrGranit <ivolation@gmail.com>
2014-06-28 15:42:02 +0200

    Function parameters no longer allow numeric characters within strings

--- src/main/java/JustSomething.java
+++ src/main/java/JustSomething.java
@@ -2,8 +2,12 @@ import java.util.function.Function;
 
 public class JustSomething {
 
-
-    public static Function<String, String> SomeMethod() {
-        return (name) -> "Hello " + name;
-    }
+	public static Function<String, String> SomeMethod() {
+		return (name) -> {
+			if (name.matches(".*\\d.*"))
+				throw new IllegalArgumentException(
+						"Input string may not contain numeric characters");
+			return "Hello " + name;
+		};
+	}
 }
--- src/test/java/JustSomethingTest.java
+++ src/test/java/JustSomethingTest.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.assertEquals;
 
 public class JustSomethingTest {
 
-    @Test
-    public void testSomeMethod() {
-        assertEquals(JustSomething.SomeMethod().apply("World!"), "Hello World!");
-    }
+	@Test
+	public void testSomeMethod() {
+		assertEquals(JustSomething.SomeMethod().apply("World!"), "Hello World!");
+	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testNumericStringInputNotAccepted() {
+		JustSomething.SomeMethod().apply("123");
+	}
 }
