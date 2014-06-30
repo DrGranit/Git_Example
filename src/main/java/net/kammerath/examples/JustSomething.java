f84c6d2f8fdf4b4ae16e37612e7e1358bff41813
DrGranit <ivolation@gmail.com>
2014-06-30 17:26:06 +0200

    removed javadocs

--- src/main/java/net/kammerath/examples/JustSomething.java
+++ src/main/java/net/kammerath/examples/JustSomething.java
@@ -4,12 +4,6 @@ import java.util.function.Function;
 
 public class JustSomething {
 
-    /**
-     * Creates a function to prepare a greeting.
-     * 
-     * @return a "Hello World" function taking 
-	 * any input and returning a strings
-     */
 	public static <T> Function<T, String> SomeMethod() {
         // Goodbye, finally
         return (name) -> "Hello " + name;
